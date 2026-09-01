efa1586b6261a601ee99ec1d67a2e90bfd0526f6
mayurpkapilit23 <mayurp@kapilit.com>
2026-09-01 15:52:21 +0530

    update book route

--- src/book_api/features/book/router.py
+++ src/book_api/features/book/router.py
@@ -8,11 +8,11 @@ from .service import book_service
 router = APIRouter()
 
 # GET endpoint to retrieve all books from the database
-@router.get("/", response_model=list[BookResponse])
+@router.get("", response_model=list[BookResponse])
 def list_books():
     return book_service.get_all_books()
 
 # POST endpoint to create a new book and return the created book with its ID
-@router.post("/", response_model=BookResponse, status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=BookResponse, status_code=status.HTTP_201_CREATED)
 def create_book(payload: BookCreate):
     return book_service.create_book(payload)
